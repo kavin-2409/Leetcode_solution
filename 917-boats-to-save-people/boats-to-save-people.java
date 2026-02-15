647d6a273debf6a39a61be8991b6d89bcb7c3bf4
kavin-2409 <akavin2024@gmail.com>
2026-02-15 22:36:18 +0530

    Time: 20 ms (81.99%) | Memory: 56.5 MB (56.44%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -1,17 +1,18 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int left=0;
-        int right=people.length-1;
-        int boats=0;
-        while(left<=right){
-            if(people[left]+people[right]<=limit){
-                left++;
-            }
-            right--;
-            boats++;
-        }
+       Arrays.sort(people);
+       int left=0;
+       int right=people.length-1;
+       int boat=0;
 
-        return boats;
+       while(left<=right){
+         if(people[left]+people[right]<=limit){
+           left++;
+          // boat++;
+         }
+         right--;
+         boat++;
+       }
+       return boat;
     }
 }
